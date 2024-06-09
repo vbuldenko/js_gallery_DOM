72771465da3984c4288192fba528bc3c8393fb56
Vladyslav <vladbuldenko@gmail.com>
2024-06-09 11:45:47 +0300

    make build

---
+++ index.d2f92972.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest("a");e.src=r.href});
+//# sourceMappingURL=index.d2f92972.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const largeImg = document.querySelector("#largeImg");
+const thumbList = document.querySelector("#thumbs");
+thumbList.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const newLargeImgLink = e.target.closest("a");
+    largeImg.src = newLargeImgLink.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
